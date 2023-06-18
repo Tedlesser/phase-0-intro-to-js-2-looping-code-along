a89dff13c167a9d3ed602120f356013af2452f15
Theodore Lesser <ted.a.lesser@gmail.com>
2023-06-18 11:21:59 -0400

    Adds myFiles

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solutions in this file
+function writeCards(Names, Occasion){
+  let messages = [];
+
+  for (let i = 0; i < Names.length; i++) {
+    let message = `Thank you, ${Names[i]}, for the wonderful ${Occasion} gift!`
+    messages.push(message);
+  }
+
+  return messages;
+}
+
+const results = writeCards(["Charlie", "Samip", "Ali"], "Birthday");
+console.log(results);
+
+  function countDown(number) {
+    if (number < 0) {
+      console.log("Number should be non-negative.");
+      return;
+    }
+
+    for (let i = number; i >= 0; i--) {
+      console.log(i);
+    }
+  }
